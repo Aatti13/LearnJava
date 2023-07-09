c553c8af91d7f308b1c6219a86d5f921767adec7
Aatti13 <86865554+Aatti13@users.noreply.github.com>
2023-07-09 14:00:45 +0530

    Add files via upload

---
+++ Basic Codes/typeconv.java
@@ -0,0 +1,12 @@
+class typeconv {
+    public static void main(String[] args) {
+        int i = 45;
+        byte b;
+        
+        System.out.println("Explicit Type Conversion int to byte");
+        b = (byte) i;
+        System.out.println(i);
+        System.out.println();
+        System.out.println(b);
+    }    
+}
