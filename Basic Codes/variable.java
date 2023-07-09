c438742f1e0c5b67fc854d0eeedf443e31c98b36
Aatti13 <86865554+Aatti13@users.noreply.github.com>
2023-07-09 13:55:47 +0530

    Add files via upload

---
+++ Basic Codes/variable.java
@@ -0,0 +1,8 @@
+public class variable {
+    public static void main(String[] args){
+        String x = "Hello World";
+        float dec_no = 11.5f;
+        System.out.println(x);
+        System.out.println(dec_no);
+    }
+}
