802723f17e44f662759a90e8c28223d545a94c3c
Aatti13 <86865554+Aatti13@users.noreply.github.com>
2023-07-10 11:05:06 +0530

    Add files via upload

---
+++ Basic Codes/operators.java
@@ -0,0 +1,8 @@
+class operators {
+    public static void main(String[] args){
+        int a = 5;
+        int result = ~5;
+        System.out.println(a);
+        System.out.println(result);
+    }    
+}
